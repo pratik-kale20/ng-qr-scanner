a6d536e66822af45c81eefa71e4a33a73ff59028
pratik-kale20 <ppvkale@gmail.com>
2022-07-21 10:10:42 +0530

    QR Result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { ModalModule } from 'ng2-modal-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ZXingScannerModule
+    ZXingScannerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
