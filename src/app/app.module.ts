a6fc6534f1dd271d33b989e93dc7d04f256dacae
pratik-kale20 <ppvkale@gmail.com>
2022-07-21 14:42:37 +0530

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { ModalModule } from 'ng2-modal-module';
 import {AngularFireModule} from '@angular/fire/compat'
@@ -15,7 +15,7 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     ZXingScannerModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
